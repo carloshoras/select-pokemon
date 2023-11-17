61b53725a1fa8f2ff5744399a90e337839d09c00
carloshoras <carloshoras>
2023-11-18 00:56:19 +0100

    Arreglado boton + css

--- js/script.js
+++ js/script.js
@@ -1,26 +1,23 @@
+const contenedor = document.querySelector('body')
+const infoPokemon = document.createElement('div')
+infoPokemon.setAttribute('class', 'infoPokemon')
+contenedor.appendChild(infoPokemon)
+
 const selector = document.getElementById('get-pokemon')
 
 selector.addEventListener('click', () => {
+    console.log("hola");
+    console.log(document.getElementById('pokemon-select').value)
     fetch(`https://pokeapi.co/api/v2/pokemon/${document.getElementById('pokemon-select').value}`)
     .then((response) => {
-        if(!response.ok) {
-            throw new Error('La solicitud no fue exitoso');
-        }
-        console.log(document.getElementById('pokemon-select').value)
-        return response.json()
+         if(!response.ok) {
+             throw new Error('La solicitud no fue exitoso');
+         }
+         return response.json()
+     })
+     .then((data) => {
+        console.log("Mundo")
+        infoPokemon.innerHTML =`<div class="infoPokemon"><img src="${data.sprites.back_default}" alt="pokemon"/><p><span>Nombre:</span> ${data.name}</p><p><span>Tipo:</span> ${data.types[0].type.name}</p><p><span>Peso:</span> ${data.weight}</p><p><span>Altura:</span> ${data.height}</p></div>`
     })
-    .then((data) => {
-        const contenedor = document.querySelector('.container');
-        contenedor.innerHTML += `
-        <div class="infoPokemon">
-            <img src="${data.sprites.back_default}"/>
-            <p>Nombre: ${data.name}</p>
-            <p>Tipo: ${data.types[0].type.name}</p>
-            <p>Peso: ${data.weight}</p>
-            <p>Altura: ${data.height}</p>
-        </div>
-        `
-    })
-
 })
 
