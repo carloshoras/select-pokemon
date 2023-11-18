46065eb14cdc78c025eb42618294a59d0ac4ac4c
carloshoras <carloshoras>
2023-11-18 02:53:46 +0100

    Quitados console.log debug

--- js/script.js
+++ js/script.js
@@ -6,8 +6,6 @@ contenedor.appendChild(infoPokemon)
 const selector = document.getElementById('get-pokemon')
 
 selector.addEventListener('click', () => {
-    console.log("hola");
-    console.log(document.getElementById('pokemon-select').value)
     fetch(`https://pokeapi.co/api/v2/pokemon/${document.getElementById('pokemon-select').value}`)
     .then((response) => {
          if(!response.ok) {
@@ -16,7 +14,6 @@ selector.addEventListener('click', () => {
          return response.json()
      })
      .then((data) => {
-        console.log("Mundo")
         infoPokemon.innerHTML =`<div class="infoPokemon"><img src="${data.sprites.back_default}" alt="pokemon"/><p><span>Nombre:</span> ${data.name}</p><p><span>Tipo:</span> ${data.types[0].type.name}</p><p><span>Peso:</span> ${data.weight}</p><p><span>Altura:</span> ${data.height}</p></div>`
     })
 })
